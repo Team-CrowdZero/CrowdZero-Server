5f6cde304cb06e51962ccd126279862ead3b6514
choubung <choubung@sookmyung.ac.kr>
2025-02-08 00:12:16 +0900

    Feat: 첫 세팅

---
+++ src/main/java/com/crowdzero/crowdzero_sever/CrowdzeroSeverApplication.java
@@ -0,0 +1,13 @@
+package com.crowdzero.crowdzero_sever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrowdzeroSeverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrowdzeroSeverApplication.class, args);
+	}
+
+}
