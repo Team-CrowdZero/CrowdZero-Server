89a1e2594435f11419da772c8c202d3e2b12535c
choubung <choubung@sookmyung.ac.kr>
2025-02-08 22:46:14 +0900

    Build: lombok 종속성 추가

--- src/main/java/com/crowdzero/crowdzero_sever/domain/Place.java
+++ src/main/java/com/crowdzero/crowdzero_sever/domain/Place.java
@@ -2,7 +2,9 @@ package com.crowdzero.crowdzero_sever.domain;
 
 import jakarta.persistence.*;
 
-@Entity
+@Entit
+@AllArgsConstructor
+@Getter
 @Table(name = "place")
 public class Place {
     @Id
@@ -18,28 +20,5 @@ public class Place {
     private String imageUrl;
 
     public Place() {
-    }
-
-    public Place(int id, String areaCd, String areaNm, String imageUrl) {
-        this.id = id;
-        this.areaCd = areaCd;
-        this.areaNm = areaNm;
-        this.imageUrl = imageUrl;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getAreaNm() {
-        return areaNm;
-    }
-
-    public String getAreaCd() {
-        return areaCd;
-    }
-
-    public String getImageUrl() {
-        return imageUrl;
     }
 }
