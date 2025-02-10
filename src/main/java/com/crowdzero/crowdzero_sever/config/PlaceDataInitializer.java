9542dc976c1b42daf71263b5d73fb874b670afd2
choubung <choubung@sookmyung.ac.kr>
2025-02-10 18:06:10 +0900

    Refactor: 장소 테이블에서 이미지 url 삭제

--- src/main/java/com/crowdzero/crowdzero_sever/config/PlaceDataInitializer.java
+++ src/main/java/com/crowdzero/crowdzero_sever/config/PlaceDataInitializer.java
@@ -20,11 +20,11 @@ public class PlaceDataInitializer implements CommandLineRunner {
     public void run(String... args) {
         List<Place> places = Arrays.asList(
                 // TODO: 프론트에서 imageUrl 받기
-                new Place(1, "강남역", "POI014", ""),
-                new Place(2, "광화문 광장", "POI088", ""),
-                new Place(3, "삼각지역", "POI030", ""),
-                new Place(4, "서울역", "POI033", ""),
-                new Place(5, "여의도", "POI072", "")
+                new Place(1, "강남역", "POI014"),
+                new Place(2, "광화문 광장", "POI088"),
+                new Place(3, "삼각지역", "POI030"),
+                new Place(4, "서울역", "POI033"),
+                new Place(5, "여의도", "POI072")
         );
 
         // 초기 데이터가 없는 경우에만 저장
