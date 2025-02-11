c99caa5a8be6e92d1d36fdc297c5ee4f8c38a129
choubung <choubung@sookmyung.ac.kr>
2025-02-11 20:14:35 +0900

    Feat: 요청 당시 날짜와 장소 아이디를 기반으로한 도로 통제 데이터 반환 기능 추가

--- src/main/java/com/crowdzero/crowdzero_sever/accidentApi/domain/AccidentRepository.java
+++ src/main/java/com/crowdzero/crowdzero_sever/accidentApi/domain/AccidentRepository.java
@@ -1,8 +1,14 @@
 package com.crowdzero.crowdzero_sever.accidentApi.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface AccidentRepository extends JpaRepository<Accident, Long> {
+    @Query("SELECT a FROM Accident a WHERE a.area.id = :areaId AND a.acdntOccrDt LIKE CONCAT(:currentDate, '%')")
+    List<Accident> findByAreaIdAndCurrentDate(@Param("areaId") Long areaId, @Param("currentDate") String currentDate);
 }
